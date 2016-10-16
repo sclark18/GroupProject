2f1f794bf990b283e4bf95da233db1911a2d4e92
NS2K <christopherhayward@mail.com>
2016-10-16 19:48:02 +0100

    Add files via upload
    Fixed issues caused by my poor dictionary usage.
    Finished level_up() function, which is automatically called by player_update().
    Fixed issues related to usage of the random module.

--- player_functions.py
+++ player_functions.py
@@ -1,31 +1,32 @@
-import math.random
+import math
+import random
 # Work in Progress!
 
-player = {"name" : player_name, "Hit Points" : current_health, "Max Hit Points" : max_health, "alive" : True, "EXP" : current_exp, "EXP to next level" : to_next_level, "level" : 1, "Attack Points" : atk, "Defense Points" : def_p}
+player = {"name" : "", "Hit Points" : 0, "Max Hit Points" : 0, "alive" : True, "EXP" : 0, "EXP to next level" : 0, "level" : 1, "Attack Points" : 0, "Defense Points" : 0}
 # Values for the player, should be self explanatory.
-# When displaying the amount of EXP to the next level to the player, it should be to_next_level - current exp
+# When displaying the amount of EXP to the next level to the player, it should be player["EXP to next level"] - player["EXP"]
 
 def	player_gen(name="player"):
-	player_name = name
-	max_health = 13 + random.randint(1,13)
-	current_health = max_health
-	current_exp = 0
-	to_next_level = 12
-	atk = 5 + random.randint(1,10)
-	def_p = 0 + random.randint(1,5)
+	player["name"] = name
+	player["Max Hit Points"] = 13 + random.randint(1,13)
+	player["Hit Points"] = player["Max Hit Points"]
+	player["EXP"] = 0
+	player["EXP to next level"] = 12
+	player["Attack Points"] = 5 + random.randint(1,10)
+	player["Defense Points"] = 0 + random.randint(1,5)
 	return
 # This function is called once at the start of the game and assigns semi-random points to each variable. Feel free to change the points to create a better game balance when we get there, they are basically arbitrary.
 # The player's name is taken for the scoreboard, but should default to player if left blank
 
 def player_update(dmg = 0, heal = 0, xp = 0):
-	current_health -= dmg
-	current_health += heal
-	if current_health <= 0:
+	player["Hit Points"] -= dmg
+	player["Hit Points"] += heal
+	if player["Hit Points"] <= 0:
 		player["alive"] = False
-	else if current_health > max_health:
-		current_health = max_health
-	current_exp += xp
-	if current_exp >= to_next_level:
+	elif player["Hit Points"] > player["Max Hit Points"]:
+		player["Hit Points"] = player["Max Hit Points"]
+	player["EXP"] += xp
+	if player["EXP"] >= player["EXP to next level"]:
 		level_up()
 	return
 # This function should be called when the players temporary stats need to be updated, such as when in combat or using a healing item
@@ -33,14 +34,30 @@ def player_update(dmg = 0, heal = 0, xp = 0):
 
 def level_up():
 	hp = player["level"] + random.randint(0, player["level"])
-	current_health += hp
-	max_health += hp
-	atk += player["level"] + random.randint(0, (player["level"]/2).floor())
-	def_p += player["level"] + random.int(0, (player["level"]/4).floor())
+	player["Hit Points"] += hp
+	player["Max Hit Points"] += hp
+	player["Attack Points"] += player["level"] + random.randint(0, (math.floor(player["level"]/2)))
+	player["Defense Points"] += player["level"] + random.randint(0, (math.floor(player["level"]/4)))
 	player["level"] = player["level"] + 1
-	current_exp = to_next_level - current_exp
-	to_next_level = player["level"] + to_next_level + 12
+	player["EXP"] = player["EXP to next level"] - player["EXP"]
+	player["EXP to next level"] = player["level"] + player["EXP to next level"] + 12
 	return
 # This function is called by player_update when the player is due to level up
 # This could be updated later in development to print the player's updated stats and allow the player more choice in which stats are upgraded
 # The stat upgrades are all prospective and fairly arbitrary and you should all feel free to change them for better game balance when we get there
+
+""" I used these tests to check these functions worked and it looked alright to me so it should be in working order.
+player_gen()
+
+print(player)
+
+player_update(2,0,0)
+
+print(player)
+
+player_update(xp = 12)
+
+print(player)
+
+input("enter to exit!!!")
+"""
