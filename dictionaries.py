b56f5523235649b52efa232d58a6416b2e786968
karcik <karolina.hornikova7@gmail.com>
2016-10-17 12:43:51 +0100

    Add files via upload

---
+++ dictionaries.py
@@ -0,0 +1,218 @@
+weapons_sword = { 
+	"name": "Sword",
+	"description": "This is a sword made from the finest metal that can be found, handcrafted by the most skilled blacksmith."
+
+				}
+
+weapons_crossbow = {
+	"name": "Crossbow",
+	"description": "A crossbow renowned for it's precision and power."
+			
+}
+
+weapons_pistol = {
+	"name": "Pistol",
+	"description": "A military grade pistol known for it's portability and accuracy."
+			
+}
+
+weapons_battleaxe = {
+	"name": "Battle Axe",
+	"description": "A Battle Axe that inflicts heavy damage to the enemies"
+	
+}
+
+weapons_meatcleaver= {
+	"name": "Meat Cleaver",
+	"description": "Butcher's finest Meat Cleaver with the sharpest edge to pierce through anything in it's way."
+			
+}
+
+weapons_bowandarrow = {
+	"name": "Bow and Arrow",
+	"description": "Robin Hood's Bow and Arrow will give you the ultimate aim to kill."
+			
+}
+
+weapons_warhammer = {
+	"name": "War Hammer",
+	"description": "Nothing better than Thor's original War Hammer, Mjolnir!"
+			
+}
+
+weapons_poisondarts = {
+	"name": "Poison Darts",
+	"description": "Poision darts that are stealthy but requires precision and technique to use it."
+			
+}
+
+weapons_flamethrower = {
+	"name": "Flamethrower",
+	"description": "A powerful flamethrower that has a range of 10 ft known for its capabilities to brun everything to ashes."
+			
+}
+
+weapons_claws = {
+	"name": "Claws",
+	"description": "Retractable claws like Wolvorine that can be used to rip apart enemies."
+			
+}
+
+weapons_grenade = {
+	"name": "Grednade",
+	"description": "A grenade that can inflict damage to the surrounding when detonated"
+			
+}
+
+weapons = { 
+			"Sword": weapons_sword,
+			"Crossbow": weapons_crossbow,
+			"Pistol": weapons_pistol,
+			"Battle Axe": weapons_battleaxe,
+			"Meat Cleaver": weapons_meatcleaver,
+			"Bow and Arrow": weapons_bowandarrow,
+			"War Hammer": weapons_warhammer,
+			"Poison Darts": weapons_poisondarts,
+			"Flamethrower": weapons_flamethrower,
+			"Claws": weapons_claws,
+			"Grenade": weapons_grenade
+}
+
+monsters_goblin = {
+	"name": "Goblin",
+	"description": "1 metre tall, green'skinned, hideous humanoid that blocks your path."
+}
+
+monsters_firedragon = {
+	"name": "Fire Dragon",
+	"description": "A creature with long, powerful limbs that end in sharp claws blocks your path. It's a Fire Daragon! Breaths out 10 metre long flames, which would burn you to ashes. His extreme size"
+}
+	
+monsters_clown = {
+	"name": "Killer Clown",
+	"description": "Run for your life!!!"
+}
+	
+monsters_voldemort = {
+	"name": "Voldemort",
+	"description": "Some say he his horrible nose job is the source of his powers. Beware, he might try and steal your nose!"
+}
+	
+monsters_werewolf = {
+	"name": "Werewolf",
+	"description": "Legend says the werewolf was once an aware wolf."
+}
+	
+monsters_vampire = {
+	"name": "Vampire",
+	"description": "Lives to take the lives of the innocent to quench his thirst for blood, the solitary vampire."
+}
+	
+monsters_medusa = {
+	"name": "Medusa",
+	"description": "Kim Kardashian with snake hair, just make sure not to look into her eyes."
+}
+	
+monsters_zombie = {
+	"name": "Zombie",
+	"description": "They love brains..."
+}
+	
+monsters_yeti = {
+	"name": "Yeti",
+	"description": "Yeti also known as the Abominable Snowman, only a few live to tell the tale of their encounter with one."
+}
+	
+monsters_godzilla = {
+	"name": "Godzilla",
+	"description": "You know who he/she is!!"
+}
+
+monsters = {
+			"Goblin": monsters_goblin,
+			"Fire Dragon": monsters_firedragon,
+			"Killer Clown": monsters_clown,
+			"Voldemort": monsters_voldemort,
+			"Werewolf": monsters_werewolf,
+			"Vampire": monsters_vampire,
+			"Medusa": monsters_medusa,
+			"Zombie": monsters_zombie,
+			"Yeti": monsters_yeti,
+			"Godzilla": monsters_godzilla
+}
+
+
+
+adjectives = [advanced, ancient, atomic, awesome, best, big, biological, bladed, dangerous, deadly, destructive, 
+			effective, efficient, excellent, good, great, handy, heavy, handcrafted, huge, invincible, large, laser, 
+			lethal, light, portable, loaded, magical, massive, military, modern,  new, old, ordinary, potent, powerful, 
+			primitive, sacred, sharp, single, small, special, splendid, standard, strong, superior, traditional, ultimate, 
+			unique, wooden, imperial, supreme, allmighty, legendary]
+
+potions_healthboost = {
+	"name": "Health Boost"
+	"description": "Increases your health and you will definitely need that later on!"
+
+}
+
+potions_steroids = {
+	"name": "Steroids"
+	"description": "Makes you stronger, bigger and cooler."
+
+}
+
+potions_superhealthboost = {
+	"name": "Super Health Boost"
+	"description": "Restores your health completely!!"
+
+}
+
+potions_magicmushrooms = {
+	"name": "Magic Mushrooms"
+	"description": "???"
+
+}
+
+potions = {
+	"Health Boost": potions_healthboost,
+	"Steroids": potions_steroids,
+	"Super Health Boost": potions_superhealthboost,
+	"Magic Mushrooms": potions_magicmushrooms
+}
+
+equipment_platearmour = {
+	"name": "Plate Armour"
+	"description": "Provides almost perfect protection, yet it's very heavy and uncomfortable."
+
+}
+
+equipment_chainmail = {
+	"name": "Chainmail Armour"
+	"description": "Perfection against all! However, there is limited mobility."
+	
+}
+
+equipment_mankini = {
+	"name": "Mankini"
+	"description": "You would be crazy not to want it. Brilliant comfort and protection against monsters"
+	
+}
+
+equipment_magicalrobe = {
+	"name": "Magical Robe"
+	"description": "Provides bursts of invisibility so you can evade attacks, to take less damage."
+	
+}
+
+equipment = {
+	"Plate Armour": equipment_platearmour,
+	"Chainmail Armour": equipment_chainmail,
+	"Mankini": equipment_mankini,
+	"Magical Robe": equipment_magicalrobe
+}
+
+
+
+
+
+
